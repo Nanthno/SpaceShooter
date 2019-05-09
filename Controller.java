b1510a5379fbf6dfb9727848e7e33cd119967e27
nanthno <midnightquack@gmail.com>
2019-05-09 17:56:55 -0500

    create a game loop in the Controller

--- Controller.java
+++ Controller.java
@@ -1,11 +1,39 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 class Controller {
 
     static GraphicsManager graphicsManager;
+
+    static ArrayList<EnemyShip> enemyShips = new ArrayList<EnemyShip>();
+
+    static final int updatesPerSecond = 60;
     
     public static void main(String[] args) {
 
 	graphicsManager = new GraphicsManager();
 
+	gameLoop();
+
+    }
+
+    static void update() {
+	graphicsManager.drawScreen();
+    }
+
+    public static void gameLoop() {
+	int tickCount = 0;
+	long time = (new Date()).getTime();
+	long startTime = time;
+	while(true) {
+	    update();
+	    tickCount++;
+
+	    while(tickCount*(1000/updatesPerSecond) > time-startTime) {
+		time = (new Date()).getTime();
+	    }
+	
+	}
+	
     }
 }
-	
